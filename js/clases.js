4b383f35dfb3a0c09727792b7a8eb7e5ff04bb47
‌jguerra56 <joaquin.guerra56@gmail.com>
2025-11-03 05:39:08 -0300

    FEAT: Se crea registro cliente, manejo de secciones, mejora login. Crea administradores, comprobacion de formulario registro

--- js/clases.js
+++ js/clases.js
@@ -1,53 +1,120 @@
 class Sistema {
     constructor(){
-        this.listaDeUsuarios = [
-            new Usuario("Joaquín", "Guerra", "joaco", "1234",0),
-            new Usuario("123", "123", "jj", "1234",1)
+        this.listaDeClientes = [
+            new Cliente("Joaquín", "Guerra", "joaco", "1234",10000),
+            new Cliente("123", "123", "jj", "1234",10000)
 
         ]
+        this.listaDeAdministradores = [
+            new Administrador("admin","1234"),
+            new Administrador("administrador","admin123")
+        ];
         this.usuarioLogueado = null;
     }
 
-    obtenerTipoDeLogin(user,pass){
-    let usuarios = this.listaDeUsuarios;
+    existeUsuario(user,pass){
+    let clientes = this.listaDeClientes;
+    let administradores = this.listaDeAdministradores;
     let usuarioEncontrado = null;
-    for (let i = 0; i < usuarios.length; i++) {
-        let usuario = usuarios[i];
-        if (usuario.email === user && usuario.password === pass) {
-            usuarioEncontrado = usuario;
-            break;
+    for (let i = 0; i < clientes.length; i++) {
+        let cliente = clientes[i];
+        if (cliente.nombreUsuario === user && cliente.password === pass) {
+            usuarioEncontrado = cliente;
+            return 1; //Usuario tipo cliente
         }
     }
 
-    if (usuarioEncontrado) {
-        if(usuarioEncontrado.rol == 0){
-            this.usuarioLogueado = usuarioEncontrado;
-            return 0;
-            
-        }else{
-            this.usuarioLogueado = usuarioEncontrado;
-            return 1;
-            
+        for (let i = 0; i < administradores.length; i++) {
+            let admin = administradores[i];
+            if (admin.nombreUsuario === user && admin.password === pass) {
+                usuarioEncontrado = admin;
+                return 0; //Usuario tipo administrador
         }
-    } else {
+        }
+    return -1; //Usuario no encontrado
+    }
+
+    obtenerUsuarioPorNombreUsuario(nombreUsuario){
+        for (let cliente of this.listaDeClientes) {
+            if (cliente.nombreUsuario === nombreUsuario) {
+                return cliente;
+            }
+        }
+
+        for (let admin of this.listaDeAdministradores) {
+            if (admin.nombreUsuario === nombreUsuario) {
+                return admin;
+            }
+        }
         return null;
     }
+    guardarUsuarioLogueado(nombreUsuario){
+        let usuario = this.obtenerUsuarioPorNombreUsuario(nombreUsuario);
+        if(usuario == null){
+            console.log("Usuario no encontrado");
+            return;
+        }       
+        this.usuarioLogueado = usuario;
     }
 
-    obtenerUsuarios(){
-        return this.listaDeUsuarios;
+    obtenerClientes(){
+        return this.listaDeClientes;
+    }
+
+    comprobarContrasenia(contrasenia){
+        if(contrasenia.length < 5){
+            alert("La contraseña debe tener al menos 5 caracteres.");
+            return false;
+        }
+        let tieneMayuscula = false;
+        let tieneMinuscula = false;
+        let tieneNumero = false;
+        for(let i=0; i<contrasenia.length; i++){
+             // Verifica si es una letra mayúscula
+        if (contrasenia[i] >= "A" && contrasenia[i] <= "Z") {
+            tieneMayuscula = true;
+        }
+
+        // Verifica si es una letra minúscula
+        else if (contrasenia[i] >= "a" && contrasenia[i] <= "z") {
+            tieneMinuscula = true;
+        }
+            if(!isNaN(contrasenia[i])){
+                tieneNumero = true;
+            }
+        }
+        if(!tieneMayuscula || !tieneMinuscula || !tieneNumero){
+            alert("La contraseña debe contener al menos una letra mayúscula, una letra minúscula y un número.");
+            return false;
+        }
+        return true;
+    }
+
+    crearCliente(nombre, apellido, nombreUsuario, password){
+        let nuevoCliente = new Cliente(nombre, apellido, nombreUsuario, password,10000);
+        this.listaDeClientes.push(nuevoCliente);
     }
 }
 
-let idUsuario = 0;
-class Usuario {
-    constructor(nombre, apellido, email, password,rol) 
+let idCliente = 1;
+class Cliente {
+    constructor(nombre, apellido, nombreUsuario, password,saldo) 
     {
-        this.id = idUsuario++;
+        this.id = idCliente++;
         this.nombre = nombre;
         this.apellido = apellido;
-        this.email = email;
+        this.nombreUsuario = nombreUsuario;
         this.password = password;
-        this.rol = rol ?? 0; // 0: usuario, 1: admin
+        this.saldo = saldo;
+    }
+}
+
+let idAdministrador = 1;
+class Administrador {
+    constructor(nombreUsuario, password) {
+        this.id = idAdministrador++;
+        this.nombreUsuario = nombreUsuario;
+        this.password = password;
+
     }
 }
--- js/code.js
+++ js/code.js
@@ -1,43 +1,72 @@
 let sistema = new Sistema();
 
-
-function login() {
-    let username = document.getElementById("username").value;
-    let password = document.getElementById("password").value;
-    let login = sistema.obtenerTipoDeLogin(username, password);
-    switch (login) {
-        case 0:
-            console.log("Es admin");
-            console.log("usuario logueado es "+sistema.usuarioLogueado.nombre);
-
-            document.querySelector("#seccionAdmin").style.display = "block";
-            document.querySelector("#contenedorLogin").style.display = "none";
-            llenarTablaUsuarios();
-            
-            break;
-        case 1:
-            console.log("Es usuario");
-            console.log("usuario logueado es "+sistema.usuarioLogueado.nombre);
-            break;
-        default:
-            console.log("Credenciales no son validas");
-            break;
+function mostrarSeccion(idSeccion){
+    const secciones = document.querySelectorAll(".seccion");
+    for(let seccion of secciones) 
+    {
+        seccion.style.display = "none";
     }
 
-    function llenarTablaUsuarios(){
-        let tablaUsuarios = document.getElementById("tablaUsuarios");
-        let usuarios = sistema.obtenerUsuarios();
-        let filas ="";
-        for (let usuario of usuarios) {
-            filas += `
-                <tr>
-                    <td>${usuario.id}</td>
-                    <td>${usuario.nombre}</td>
-                    <td>${usuario.email}</td>
-                </tr>
-            `;
-        }
-        tablaUsuarios.innerHTML = filas;
-    }
+    document.querySelector(`#${idSeccion}`).style.display = "block";
 }
 
+function login() {
+  let username = document.getElementById("username").value;
+  let password = document.getElementById("password").value;
+
+  let usuario = sistema.existeUsuario(username, password);
+  if(usuario == 0) {
+    alert("Login exitoso como administrador");
+    sistema.guardarUsuarioLogueado(username);
+    mostrarSeccion("seccionAdmin");
+    llenarTablaClientes();
+    return;
+  }
+  if (usuario == 1) {
+    alert("Login exitoso como cliente");
+    sistema.guardarUsuarioLogueado(username);
+    mostrarSeccion("seccionCliente");
+    llenarTablaClientes();
+  } else {
+    alert("Credenciales inválidas");
+  }
+}
+
+function llenarTablaClientes() {
+  let tablaClientes = document.getElementById("tablaClientes");
+  let clientes = sistema.obtenerClientes();
+  let filas = "";
+  for (let cliente of clientes) {
+    filas += `
+                <tr>
+                    <td>${cliente.id}</td>
+                    <td>${cliente.nombre}</td>
+                    <td>${cliente.apellido}</td>
+                </tr>
+            `;
+  }
+  tablaClientes.innerHTML = filas;
+}
+
+function registrarCliente() {
+  let form = document.querySelector("#registroCliente");
+  if (!form.reportValidity()) {
+    console.log("Formulario no válido");
+    return;
+  }
+  let nombre = form.nombre.value;
+  let apellido = form.apellido.value;
+  let usuario = form.usuario.value;
+  let password = form.password.value;
+  if(sistema.obtenerUsuarioPorNombreUsuario(usuario)!=null){
+    alert("El nombre de usuario ya existe. Por favor, elija otro.");
+    return;
+  }
+    if(!sistema.comprobarContrasenia(password)){
+        return;
+    }
+    sistema.crearCliente(nombre, apellido, usuario, password);
+    alert("Cliente registrado exitosamente");
+    form.reset();
+    
+}
